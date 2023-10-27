b2904b0d222ffef2a2b3a3d4de25fac1e9cce4a0
Arran Strange <arranstrange@googlemail.com>
2023-10-27 13:43:31 +0100

    body amendment

--- src/components/our-work/index.tsx
+++ src/components/our-work/index.tsx
@@ -3,14 +3,14 @@ import './styles.css';
 function OurWork() {
   return (
     <div className="our-work">
-      <body className="our-work-content body">
+      <p className="our-work-content body">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque feugiat in elit a
         dapibus. Curabitur odio quam, convallis in vulputate nec, fringilla sit amet nibh. Cras
         elementum, turpis sit amet lacinia interdum, massa ligula congue dolor, eget suscipit tortor
         ante et dolor. Nullam eu nisi justo. Pellentesque fermentum ac ex eget pharetra. Proin
         fermentum mauris ac lacus egestas, a iaculis felis tristique. Cras sit amet neque quam. Nam
         egestas quis augue at condimentum.
-      </body>
+      </p>
       <div className="our-work-header">
         OUR <br />
         WORK
