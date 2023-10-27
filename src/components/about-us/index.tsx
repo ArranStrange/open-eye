e65c3fd5e7cdd2303829f0cddc8db7c667d09a5f
Archie Strange <archie.strange@wundermanthompson.com>
2023-10-27 13:47:36 +0100

    Declare mp4 type

--- src/components/about-us/index.tsx
+++ src/components/about-us/index.tsx
@@ -3,11 +3,8 @@ import './styles.css';
 function AboutUs() {
   return (
     <div className="about-us">
-      <div className="header">
-        ABOUT <br />
-        US
-      </div>
-      <body className="about-us-content body">
+      <div className="header">ABOUT US</div>
+      <p className="about-us-content body">
         We're Saph and Luke, Motion and Graphics Guys. Check out the video above, it's not ours, but
         it's cool. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque feugiat in
         elit a dapibus. Curabitur odio quam, convallis in vulputate nec, fringilla sit amet nibh.
@@ -15,7 +12,7 @@ function AboutUs() {
         tortor ante et dolor. Nullam eu nisi justo. Pellentesque fermentum ac ex eget pharetra.
         Proin fermentum mauris ac lacus egestas, a iaculis felis tristique. Cras sit amet neque
         quam. Nam egestas quis augue at condimentum.
-      </body>
+      </p>
     </div>
   );
 }
--- src/components/main-video/index.tsx
+++ src/components/main-video/index.tsx
@@ -1,10 +1,12 @@
+import videoSource from '../assets/test-title-video.mp4';
 import './styles.css';
 
 function VideoComponent() {
   return (
     <div className="video-container">
-      <video className="video-content" autoPlay controls loop>
-        <source src="./components/assets/test-title-video.mp4" type="video/mp4" />
+      <video className="video-content" autoPlay muted loop>
+        <source src={videoSource} type="video/mp4" />
+        Your browser does not support the video tag.
       </video>
     </div>
   );
