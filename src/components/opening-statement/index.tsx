13c3eec383c936b0b6eec50c36616780332a19c0
Arran Strange <arranstrange@googlemail.com>
2023-11-03 15:23:36 +0000

    offset-y hook added

--- src/components/opening-statement/index.tsx
+++ src/components/opening-statement/index.tsx
@@ -1,16 +1,31 @@
+import { useScrollPosition } from '../../hooks/use-offset-y';
 import blackIcon from '../assets/Icons/black.svg';
 import yellowIcon from '../assets/Icons/yellow.svg';
 import './styles.css';
 
 function OpeningStatement() {
+  const offsetY = useScrollPosition();
+
   return (
     <div className="opening-statement-div">
-      <img className="black-icon" src={blackIcon}></img>
+      <img
+        className="black-icon"
+        src={blackIcon}
+        style={{
+          transform: `translateY(-${offsetY * 0.5}px) rotate(${offsetY * 0.2}deg)`,
+        }}
+      />
       <p className="opening-statement">
         We add motion to both new, and established brands, and offer new ways to communicate with
         vision.
       </p>
-      <img className="yellow-icon" src={yellowIcon}></img>
+      <img
+        className="yellow-icon"
+        src={yellowIcon}
+        style={{
+          transform: `translateY(-${offsetY * 0.5}px) rotate(-${offsetY * 0.2}deg)`,
+        }}
+      />
     </div>
   );
 }
