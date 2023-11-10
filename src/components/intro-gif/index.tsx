182b8a3689fe593eae86ee3f9e029751c48032db
Arran Strange <arranstrange@googlemail.com>
2023-11-10 11:14:40 +0000

    intro GIF fade out animation added

--- src/components/intro-gif/index.tsx
+++ src/components/intro-gif/index.tsx
@@ -5,18 +5,20 @@ import './styles.css';
 
 function IntroGif() {
   const [isVisible, setIsVisible] = useState(true);
-  const setTime = 4000;
+  const setTime = 3400;
+  const [zIndex, setZIndex] = useState(999);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsVisible(false);
+      setZIndex(-999);
     }, setTime);
 
     return () => clearTimeout(timeout);
   }, []);
 
   return (
-    <div className="intro-gif-div">
+    <div style={{ zIndex: zIndex }} className="intro-gif-div fade-out">
       {isVisible && <img className="intro-gif" alt="intro-pop-up-gif" src={introGif} />}
     </div>
   );
